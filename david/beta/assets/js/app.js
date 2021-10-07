12a3e3a73f959aa3a157c0099bac60e9c4a06f73
Stuart Homfray <stuart.homfray@w3w.cz>
2021-10-07 14:14:33 +0200

    WIP

--- david/beta/assets/js/app.js
+++ david/beta/assets/js/app.js
@@ -1,21 +1,26 @@
 var app = {
+    content: document.getElementById('mainContent'),
+    template: document.getElementById('flashItem'),
     canRun: function () {
         return window.fetch && 'content' in document.createElement('template');
     },
     process: function (data) {
         console.log(data);
+        var content = !!app.content ? app.content : document.getElementById('mainContent'),
+            template = !!app.template ? app.template : document.getElementById('flashItem');
+
     },
     data: function (callback) {
         fetch('items.json')
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (json) {
-                callback(json);
-            })
-            .catch(function () {
-                callback({});
-            });
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            callback(json);
+        })
+        .catch(function () {
+            callback({});
+        });
     },
     init: function () {
         if (!app.canRun()) {
