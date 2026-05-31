a40b27162819f1aeb2424d862a190b7c72fee8cb
Stuart Homfray <stuart.homfray@w3w.cz>
2026-05-31 11:41:47 +0200

    Remove all but files

--- OpenSpritz/Bookmarklet.js
+++
@@ -1,1 +0,0 @@
-javascript:(function(){ cb = function(){ create_spritz();}; var script=document.createElement('SCRIPT');script.src='https://jackplug.github.io/OpenSpritz/spritz.js?callback=cb?callback=cb'; script.onload=cb; document.body.appendChild(script);})();
--- ronaldo/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('keydown', function(event) {
-    if (event.key === 'p' || event.key === 'P') {
-        document.body.classList.toggle('punch');
-    }
-});
