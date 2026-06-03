ba573903e70347882a322df74ec129444c24798b
Stuart Homfray <stuart.homfray@w3w.cz>
2026-06-03 17:36:39 +0200

    fixup! Wikipedia version 2 - modify UA string (WITH TEST)

--- server.js
+++ server.js
@@ -20,10 +20,10 @@ async function scrapeWorldCupResults() {
       headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.6045.9 Safari/537.36' }
     });
 
-    console.log(data);
-
     const $ = cheerio.load(data);
     const results = [];
+
+    console.log($);
 
     $('table.footballbox').each((i, table) => {
       const homeTeam = $(table).find('.fhome').text().trim();
